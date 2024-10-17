358e854e77a1d4cd7848cc52df7f7601b71d2128
Tylersadler3 <tyler.sadler@hgs.hiddengeniusproject.org>
2024-10-16 17:54:22 -0700

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,7 @@
-function darkMode() {
-// Your code here.
-}
+function counter() {
+    // Write your code below. Hint: you can finish it in about 3-5 lines.
+    //Create a variable and store the tage with the id 'counter-number' inside it
+        counter_number = document.getElementById('counter-number');
+    //Accesses the p tag and adds 1 to its number
+        counter_number.innerHTML = parseInt(counter_number.innerHTML) + 1;
+    }
